9a8bcabc35b9ad30c9b8d7c9f24688c56b311d12
Shakkira <shakkirarijam@xminds.com>
2024-10-29 14:49:44 +0530

    Pushing Grade system

--- SHAKKIRA/src/HelloWorld/Scanningmark.java
+++ SHAKKIRA/src/HelloWorld/Scanningmark.java
@@ -1,5 +1,30 @@
 package HelloWorld;
-
+import java.util.Scanner;
 public class Scanningmark {
-
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter marks");
+		int marks = sc.nextInt();
+		System.out.println(marks);
+		if(marks>80) {
+		   System.out.println("A+");
+		}
+		else if(marks>70& marks<=80) {
+			System.out.println("A");
+		}
+		else if(marks>60& marks<=70) {
+			System.out.println("B+");
+			}
+		else if(marks<=60&marks>=50) {
+			System.out.println("B");
+				}
+		else if(marks<50&marks>=40) {
+			System.out.println("c");
+		}
+		else {
+			System.out.println("D");
+		}
+		
+}
 }
